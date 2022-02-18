49cb82c2a85aa9f88a64e37b687999940706cc22
shubham sahoo <56964646+shubham9793@users.noreply.github.com>
2022-02-18 10:33:47 +0530

    Update SqlAssignment.sql

--- SqlAssignment.sql
+++ SqlAssignment.sql
@@ -10,17 +10,27 @@ CREATE TABLE Trainer_Info (
   Trainer_Password varchar(10)
 );
 
+select * from Trainer_Info;
+desc Trainer_Info;
+
 create table Batch_Info(
   Batch_Id	varchar(20) primary key,
   Batch_Owner	varchar(25),
   Batch_BU_Name	varchar(25)
 );
 
+
+desc Batch_Info;
+
+
 create table Module_Info(
   Module_Id	varchar(10) primary key,
   Module_Name	varchar(20),
   Module_Duration	Int(10)
 );
+
+
+desc Module_Info;
 
 create table Associate_Info(
   Associate_Id	varchar(10) primary key,
@@ -33,11 +43,18 @@ create table Associate_Info(
   Associate_Password varchar(10)
 );
  
+ desc Associate_Info;
+ 
+ 
 create table Questions(
   Question_Id	varchar(20) primary key,
   Module_Id	varchar(20),
   Question_Text	varchar(500)
 );
+
+
+desc Questions;
+
 
 create table Associate_Status(
   Associate_Id varchar(10) ,
@@ -49,6 +66,8 @@ create table Associate_Status(
  
 );
 
+desc Associate_Status;
+
 create table Trainer_Feedback(
   Trainer_Id	varchar(10) primary key,
   Question_Id	varchar(20),
@@ -56,6 +75,7 @@ create table Trainer_Feedback(
   Module_Id	varchar(20),
   Trainer_Rating Int (10)
 );
+desc Trainer_Feedback;
 
 create table Associate_Feedback (
   Associate_Id varchar(10) primary key,
@@ -64,7 +84,11 @@ create table Associate_Feedback (
   Associate_Rating Integer(11)
 );
 
+desc Associate_Feedback;
+
 create table Login_Details(
   User_Id	varchar(20) primary key,
   User_Password	varchar(10)
 );
+
+desc Login_Details;
